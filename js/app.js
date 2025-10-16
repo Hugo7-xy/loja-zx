7000834adafd62e2345ba5f28ecaa5aa191070bc
Hugo Moisés <hugojvc49@gmail.com>
2025-10-16 04:35:46 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@
 
 // --- 1. CONFIGURAÇÃO DO FIREBASE ---
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API,
-  authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "VITE_FIREBASE_API_KEY",
+  authDomain: "VITE_FIREBASE_AUTH_DOMAIN",
+  projectId: "VITE_FIREBASE_PROJECT_ID",
+  storageBucket: "VITE_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "VITE_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "VITE_FIREBASE_APP_ID"
 };
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
@@ -38,5 +38,6 @@ async function main() {
 }
 
 document.addEventListener('DOMContentLoaded', main);
+
 
 
